eafd9dd53db66aa10c3533235fb0b369ba305fe7
Kalyan <kalyankumarreddydoragacharla12@gmail.com>
2024-03-20 11:33:25 +0530

    first commit

--- launch_ec2.py
+++ launch_ec2.py
@@ -2,7 +2,7 @@ import boto3
  
 aws_management_console = boto3.session.Session(profile_name="default")
 ec2_console = aws_management_console.client('ec2')
-
+ 
 response = ec2_console.run_instances(
     ImageId='ami-09d3b3274b6c5d4aa',#Corrected AMI ID
     InstanceType='t2.micro',
